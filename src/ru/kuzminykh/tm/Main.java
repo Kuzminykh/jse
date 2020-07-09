e1a061f7f38761489cb16333d05e74178df36730
Natalia Kuzminykh <nata-kuz09@yandex.ru>
2020-07-09 20:52:08 +0500

    Добавлен класс с константами терминальных команд и произведен рефакторинг на блок case.

--- src/ru/kuzminykh/tm/Main.java
+++ src/ru/kuzminykh/tm/Main.java
@@ -1,6 +1,6 @@
 package ru.kuzminykh.tm;
 
-import java.util.Arrays;
+import static ru.kuzminykh.tm.constant.TerminalConst.*;
 
 public class Main {
 
@@ -13,9 +13,17 @@ public class Main {
         if (args == null) return;
         if(args.length < 1) return;
         final String param = args[0];
-        if ("version".equals(param)) displayVersion();
-        if ("about".equals(param)) displayAbout();
-        if ("help".equals(param)) displayHelp();
+        switch (param){
+            case VERSION: displayVersion();
+            case ABOUT: displayAbout();
+            case HELP: displayHelp();
+            default: displayError();
+        }
+     }
+
+    private static void displayError(){
+        System.out.println("Error! Unknown program argument...");
+        System.exit(-1);
     }
 
     private static void displayWelcome(){
